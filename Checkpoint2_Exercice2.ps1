43267e115c88d08974480f3aacaa098e0a754ccc
Ingalls666 <162147143+Ingalls666@users.noreply.github.com>
2024-05-20 16:45:21 +0200

    Add files via upload

---
+++ Checkpoint2_Exercice2.ps1
@@ -0,0 +1,64 @@
+Write-Host "--- Début du script ---"
+
+# Import du script "Function.psm1"
+
+. C:\Scripts\Functions.psm1
+
+Function Random-Password ($length = 12)
+{
+    $punc = 46..46
+    $digits = 48..57
+    $letters = 65..90 + 97..122
+
+    $password = get-random -count $length -input ($punc + $digits + $letters) |`
+        ForEach -begin { $aa = $null } -process {$aa += [char]$_} -end {$aa}
+    Return $password.ToString()
+}
+
+Function ManageAccentsAndCapitalLetters
+{
+    param ([String]$String)
+    
+    $StringWithoutAccent = $String -replace '[éèêë]', 'e' -replace '[àâä]', 'a' -replace '[îï]', 'i' -replace '[ôö]', 'o' -replace '[ùûü]', 'u'
+    $StringWithoutAccentAndCapitalLetters = $StringWithoutAccent.ToLower()
+    $StringWithoutAccentAndCapitalLetters
+}
+
+$Path = "C:\Scripts"
+$CsvFile = "$Path\Users.csv"
+$LogFile = "$Path\Log.log"
+
+$Users = Import-Csv -Path $CsvFile -Delimiter ";" -Header "prenom","nom","fonction","description" -Encoding UTF8  | Select-Object -Skip 1
+
+foreach ($User in $Users)
+{
+    $Prenom = ManageAccentsAndCapitalLetters -String $User.prenom
+    $Nom = ManageAccentsAndCapitalLetters -String $User.Nom
+    $Name = "$Prenom.$Nom"
+    If (-not(Get-LocalUser -Name "$Name" -ErrorAction SilentlyContinue))
+    {
+        $Pass = Random-Password
+        $Password = (ConvertTo-secureString $Pass -AsPlainText -Force)
+        $Description = "$($user.description) - $($User.fonction)"
+        $UserInfo = @{
+            Name                 = "$Name"
+            FullName             = "$Name"
+            Password             = $Password
+            AccountNeverExpires  = $true
+            PasswordNeverExpires = $true
+            Description          = $Description
+        }
+
+        New-LocalUser @UserInfo
+        Add-LocalGroupMember -Group "Utilisateurs" -Member "$Name"
+        Write-Host "Le compte $Name a été crée avec le mot de passe $Pass" -ForegroundColor Green
+        Log -FilePath $LogFile -Content "Le compte $Name a été crée avec le mot de passe $Pass"
+    }
+    Else
+    {
+    Write-Host "Le compte $Name existe déjà" -ForegroundColor Red
+    }
+}
+
+pause
+Write-Host "--- Fin du script ---"
